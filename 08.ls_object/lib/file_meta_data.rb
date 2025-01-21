9be4abb2abd191ddbdebca3ae17329c547c0ef0f
karlley <karlley.jp@gmail.com>
2025-01-22 07:43:20 +0900

    特殊パーミッションを1,2,4以外でも指定できるように修正

--- 08.ls_object/lib/file_meta_data.rb
+++ 08.ls_object/lib/file_meta_data.rb
@@ -21,15 +21,19 @@ class FileMetaData
     '6': 'rw-',
     '7': 'rwx'
   }.freeze
-  DIGIT_TO_INDEX = {
-    '1': -1,
-    '4': 2,
-    '2': 5
+  SPECIAL_PERMISSION_INDEX_MAP = {
+    1 => [8],
+    2 => [5],
+    3 => [5, 8],
+    4 => [2],
+    5 => [2, 8],
+    6 => [2, 5],
+    7 => [2, 5, 8]
   }.freeze
-  DIGIT_TO_PERMISSION = {
-    '1': 't',
-    '4': 's',
-    '2': 's'
+  SPECIAL_PERMISSION_CHAR = {
+    2 => 's',
+    5 => 's',
+    8 => 't'
   }.freeze
 
   def initialize(name, path)
@@ -79,13 +83,13 @@ class FileMetaData
       PERMISSION[digit.to_sym]
     end.join
     special_permission_digit = stat.mode.to_s(8)[-4]
-    special_permission_digit == '0' ? permission : add_special_permission(permission, special_permission_digit)
+    special_permission_digit == '0' ? permission : apply_special_permission(permission, special_permission_digit)
   end
 
-  def add_special_permission(permission, special_permission_digit)
-    update_index = DIGIT_TO_INDEX[special_permission_digit.to_sym]
-    special_permission = DIGIT_TO_PERMISSION[special_permission_digit.to_sym]
-    permission[update_index] = permission[update_index] == '-' ? special_permission.upcase : special_permission
+  def apply_special_permission(permission, special_permission_digit)
+    SPECIAL_PERMISSION_INDEX_MAP[special_permission_digit.to_i]&.each do |index|
+      permission[index] = permission[index] == '-' ? SPECIAL_PERMISSION_CHAR[index].upcase : SPECIAL_PERMISSION_CHAR[index]
+    end
     permission
   end
 end
